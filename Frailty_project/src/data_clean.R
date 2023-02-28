852af4150edcd9e458babbd060f7c910226d8206
Gopichand Seethi <gopichands951@gmail.com>
2023-02-27 23:12:14 -0600

    Delete Frailty_project directory

--- Frailty_project/src/data_clean.R
+++
@@ -1,6 +0,0 @@
-### Read the raw data from the folder
-raw_frailty_data <- frailty_data.csv("/Users/gopichandrseethi/Desktop/Frailty_project/data_raw/frailty_data.csv ")
-
-### clean the data 
-clean_frailty_data <- na.omit(raw_frailty_data[raw_frailty_data$Frailty != "NA",])
-
